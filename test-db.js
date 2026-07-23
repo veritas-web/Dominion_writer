a1cfbee3775c8f06bf6bd25a67d06a7b39be988d
veritas-web <admin@dominionwriter.com>
2026-07-23 22:55:37 +0530

    chore: remove unnecessary files and folders

--- test-db.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function check() {
-  const keys = await prisma.apiKey.findMany();
-  console.log('Keys in DB:', keys);
-}
-check();
